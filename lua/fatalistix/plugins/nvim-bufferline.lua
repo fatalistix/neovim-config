8b3d218bd25c1e4ea4fc174a9f14dfb868be05b7
fatalistix <slavabal11@outlook.com>
2024-02-28 16:05:38 +0700

    disabled bufferline plugin because i don't need it

--- lua/fatalistix/plugins/nvim-bufferline.lua
+++ lua/fatalistix/plugins/nvim-bufferline.lua
@@ -2,6 +2,7 @@
 -- https://github.com/akinsho/bufferline.nvim
 return {
     'akinsho/bufferline.nvim',
+    enabled = false,
     version = "*",
     dependencies = 'nvim-tree/nvim-web-devicons',
     config = function ()
