188943403710d72eeb8a19a6c6814da379f82c30
fatalistix <slavabal11@outlook.com>
2024-02-28 12:16:31 +0700

    vim options configured via lua

---
+++ lua/fatalistix/options.lua
@@ -0,0 +1,22 @@
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+vim.opt.wrap = false
+
+vim.opt.termguicolors = true
+vim.opt.cursorline = true
+
+vim.opt.scrolloff = 10
+vim.opt.signcolumn = 'number'
+vim.opt.isfname:append('@-@') -- appends '@' character
+
+vim.opt.updatetime = 1000
+vim.opt.colorcolumn = '120'
+
+vim.opt.clipboard:append('unnamedplus')
