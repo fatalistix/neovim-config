af7ce4bea5ddf248442a70f1fae8ccf4aab61715
fatalistix <slavabal11@outlook.com>
2024-02-28 21:24:57 +0700

    extended configuration (no logic has changed)

--- lua/fatalistix/plugins/nvim-tree.lua
+++ lua/fatalistix/plugins/nvim-tree.lua
@@ -3,5 +3,19 @@
 return {
     'nvim-tree/nvim-tree.lua',
     dependencies = "nvim-tree/nvim-web-devicons",
-    config = true
+    config = function()
+        local function on_attach(bufnr)
+            local api = require("nvim-tree.api")
+
+            local function opts(desc)
+                return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+            end
+
+            api.config.mappings.default_on_attach(bufnr)
+        end
+
+        require("nvim-tree").setup {
+            on_attach = on_attach,
+        }
+    end
 }
