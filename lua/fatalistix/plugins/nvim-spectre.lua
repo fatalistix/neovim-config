dd0f0b1559695f5afbcfb780bd362cffd3038586
fatalistix <slavabal11@outlook.com>
2024-02-29 11:30:20 +0700

    fast renaming in multiple files

---
+++ lua/fatalistix/plugins/nvim-spectre.lua
@@ -0,0 +1,15 @@
+-- search/replace in multiple files
+-- https://github.com/nvim-pack/nvim-spectre
+return {
+    "nvim-pack/nvim-spectre",
+    build = false,
+    cmd = "Spectre",
+    opts = { open_cmd = "noswapfile vnew" },
+    keys = {
+        {
+            "<leader>sr",
+            function() require("spectre").open() end,
+            desc = "Replace in files (Sprectre)"
+        }
+    }
+}
