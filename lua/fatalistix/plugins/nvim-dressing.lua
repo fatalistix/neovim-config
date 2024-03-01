08566d054a0c1cdd2b4cab66b4bbdda0cc7c2cdb
fatalistix <slavabal11@outlook.com>
2024-03-01 19:59:08 +0700

    ui features (currently useless)

---
+++ lua/fatalistix/plugins/nvim-dressing.lua
@@ -0,0 +1,10 @@
+-- красивые окна
+-- https://github.com/stevearc/dressing.nvim
+return {
+    "stevearc/dressing.nvim",
+    lazy = true,
+    config = function()
+        require("dressing").setup({
+        })
+    end
+}
