c4a6f3f4b7f89bffc37291f5d519ab8137bb3fc3
fatalistix <slavabal11@outlook.com>
2024-03-04 16:53:57 +0700

    vertical lines that show current indent

---
+++ lua/fatalistix/plugins/nvim-indent-blankline.lua
@@ -0,0 +1,9 @@
+-- цветные и не очень отступы как в ide (области видимости и всякое подобное)
+return {
+    "lukas-reineke/indent-blankline.nvim",
+    event = { "BufReadPost", "BufWritePost", "BufNewFile" },
+    main = "ibl",
+    config = function()
+        require("ibl").setup()
+    end,
+}
