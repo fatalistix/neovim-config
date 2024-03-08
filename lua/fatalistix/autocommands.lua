86f40c9893c3ef5a66c087e7521916ed7a649fa6
fatalistix <slavabal11@outlook.com>
2024-03-08 20:34:36 +0700

    added autoresize panels when window resizing

--- lua/fatalistix/autocommands.lua
+++ lua/fatalistix/autocommands.lua
@@ -64,10 +64,10 @@ autocmd('BufWritePre', {
 
 -- resize splits if window got resized
 autocmd({ "VimResized" }, {
-  group = augroup("resize_splits", {}),
-  callback = function()
-    local current_tab = vim.fn.tabpagenr()
-    vim.cmd("tabdo wincmd =")
-    vim.cmd("tabnext " .. current_tab)
-  end,
+    group = augroup("resize_splits", {}),
+    callback = function()
+        local current_tab = vim.fn.tabpagenr()
+        vim.cmd("tabdo wincmd =")
+        vim.cmd("tabnext " .. current_tab)
+    end,
 })
