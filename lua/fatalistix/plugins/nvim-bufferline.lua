ad6b09965ff14669ccd722e1a981a34502953d9a
fatalistix <slavabal11@outlook.com>
2024-03-08 20:33:39 +0700

    fixed incorrect command

--- lua/fatalistix/plugins/nvim-bufferline.lua
+++ lua/fatalistix/plugins/nvim-bufferline.lua
@@ -7,7 +7,7 @@ return {
     dependencies = 'nvim-tree/nvim-web-devicons',
     keys = {
         { "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", desc = "Toggle pin" },
-        { "<leader>bP", "<Cmd>BufferLineGroupClise ungrouped<CR>", desc = "Delete non-pinned buffers" },
+        { "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "Delete non-pinned buffers" },
         { "<leader>bo", "<Cmd>BufferLineCloseOthers<CR>", desc = "Delete other buffers" },
         { "<leader>br", "<Cmd>BufferLineCloseRight<CR>", desc = "Delete buffers to the right" },
         { "<leader>bl", "<Cmd>BufferLineCloseLeft<CR>", desc = "Delete buffers to the left" },
