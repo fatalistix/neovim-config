091425e7169ce9365954082f07a87c6ffffa8c83
fatalistix <slavabal11@outlook.com>
2024-03-19 14:59:19 +0700

    added mappings for fast commenting using <leader>/

--- lua/fatalistix/plugins/comment.lua
+++ lua/fatalistix/plugins/comment.lua
@@ -3,5 +3,11 @@
 return {
     'numToStr/Comment.nvim',
     event = { 'BufReadPre', 'BufNewFile' },
-    config = true -- runs require('Comment').setup()
+    lazy = false,
+    config = function()
+        require("Comment").setup()
+        local api = require("Comment.api")
+        vim.keymap.set('n', '<leader>/', function() api.toggle.linewise.count(vim.v.count > 0 and vim.v.count or 1) end, { desc = "Toggle comment line" })
+        vim.keymap.set('v', '<leader>/', "<esc><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<cr>", { desc = "Toggle comment for selection" })
+    end
 }
