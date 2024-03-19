ce5a517f6ea6e37a9c6f3253ca3a2da51fc41c8f
fatalistix <slavabal11@outlook.com>
2024-03-19 20:51:53 +0700

    trimmed whitespaces

--- lua/fatalistix/plugins/harpoon.lua
+++ lua/fatalistix/plugins/harpoon.lua
@@ -9,15 +9,15 @@ return {
         harpoon:setup()
 
         vim.keymap.set(
-            "n", 
-            "<leader>a", 
-            function() harpoon:list():append() end, 
+            "n",
+            "<leader>a",
+            function() harpoon:list():append() end,
             { desc = "Harpoon mark buffer" }
         )
         vim.keymap.set(
-            "n", 
-            "<C-e>", 
-            function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, 
+            "n",
+            "<C-e>",
+            function() harpoon.ui:toggle_quick_menu(harpoon:list()) end,
             { desc = "Harpoon marks" }
         )
 
