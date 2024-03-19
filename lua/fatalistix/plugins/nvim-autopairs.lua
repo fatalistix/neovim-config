5c0da4c4d4fc815160c20c9d4db8c9f72e0c330c
fatalistix <slavabal11@outlook.com>
2024-03-19 22:35:12 +0700

    added lazy.nvim event, simplified config

--- lua/fatalistix/plugins/nvim-autopairs.lua
+++ lua/fatalistix/plugins/nvim-autopairs.lua
@@ -2,7 +2,6 @@
 -- https://github.com/windwp/nvim-autopairs
 return {
     'windwp/nvim-autopairs',
-    config = function()
-        require('nvim-autopairs').setup()
-    end
+    event = "InsertEnter",
+    config = true,
 }
