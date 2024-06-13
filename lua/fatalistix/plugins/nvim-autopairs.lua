a5fcac79fb43513694f3215a5b4a9ab594790850
fatalistix <slavabal11@outlook.com>
2024-06-13 15:50:00 +0700

    added opts fast_wrap and disable_filetype

--- lua/fatalistix/plugins/nvim-autopairs.lua
+++ lua/fatalistix/plugins/nvim-autopairs.lua
@@ -3,5 +3,11 @@
 return {
     'windwp/nvim-autopairs',
     event = "InsertEnter",
-    config = true,
+    opts = {
+        fast_wrap = {},
+        disable_filetype = { "TelescopePrompt", "vim" },
+    },
+    config = function (_, opts)
+        require("nvim-autopairs").setup(opts)
+    end
 }
