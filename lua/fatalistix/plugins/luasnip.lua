0c6a206cafa672c517b65cbca58699b4348fa3a6
fatalistix <slavabal11@outlook.com>
2024-06-13 16:11:38 +0700

    added comment

--- lua/fatalistix/plugins/luasnip.lua
+++ lua/fatalistix/plugins/luasnip.lua
@@ -3,7 +3,7 @@
 return {
     "L3MON4D3/LuaSnip",
     dependencies = {
-        "rafamadriz/friendly-snippets",
+        "rafamadriz/friendly-snippets", -- полезные сниппеты (useful snippets)
     },
     opts = {
         history = true,
