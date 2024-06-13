961ca88b21139bf7155bdecf3d103f5847861df9
fatalistix <slavabal11@outlook.com>
2024-06-13 21:50:00 +0700

    actions preview plugin

---
+++ lua/fatalistix/plugins/actions-preview.lua
@@ -0,0 +1,12 @@
+-- Предпросмотр возможных действий (code_action, etc)
+return {
+    'aznhe21/actions-preview.nvim',
+    config = function()
+        vim.keymap.set(
+            { "v", "n" },
+            "<leader>ca",
+            require('actions-preview').code_actions,
+            { desc = "Code actions"}
+        )
+    end
+}
