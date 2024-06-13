822d1650ca8de3d3d0cbd7fbb67497d802e3f4dc
fatalistix <slavabal11@outlook.com>
2024-06-14 00:18:40 +0700

    configured showing error on insert, added 0.10.0 features

--- lua/fatalistix/plugins/lsp/lspconfig.lua
+++ lua/fatalistix/plugins/lsp/lspconfig.lua
@@ -17,72 +17,121 @@ return {
         "hrsh7th/cmp-nvim-lsp",
         "mfussenegger/nvim-jdtls"
     },
-    config = function()
+    opts = function()
+        return {
+            diagnostics = {
+                underline = true,
+                update_in_insert = true,
+                virtual_text = {
+                    -- this will set set the prefix to a function that returns the diagnostics icon based on the severity
+                    -- this only works on a recent 0.10.0 build. Will be set to "●" when not supported
+                    -- prefix = "icons",
+                    spacing = 4,
+                    source = "if_many",
+                    prefix = "●",
+                },
+                severity_sort = true,
+                -- change the Diagnostic symbols in the sign column (gutter)
+                signs = {
+                    text = {
+                        [vim.diagnostic.severity.ERROR] = " ",
+                        [vim.diagnostic.severity.WARN] = " ",
+                        [vim.diagnostic.severity.HINT] = " ",
+                        [vim.diagnostic.severity.INFO] = " ",
+                    },
+                },
+            },
+            -- Enable this to enable the builtin LSP inlay hints on Neovim >= 0.10.0
+            -- Be aware that you also will need to properly configure your LSP server to
+            -- provide the inlay hints.
+            inlay_hints = {
+                enable = true,
+                exclude = { "vue" },
+            },
+            -- Enable this to enable the builtin LSP code lenses on Neovim >= 0.10.0
+            -- Be aware that you also will need to properly configure your LSP server to
+            -- provide the code lenses.
+            codelens = {
+                enable = true,
+            },
+            -- Enable lsp cursor word highlighting
+            document_highlight = {
+                enable = true
+            },
+            -- options for vim.lsp.buf.format
+            format = {
+                formatting_options = nil,
+                timeout_ms = nil,
+            },
+            -- LSP Server Settings
+            ---@type lspconfig.options
+            -- servers = {
+            --
+            -- },
+            setup = {
+            },
+        }
+    end,
+    config = function(_, opts)
+        vim.diagnostic.config(opts.diagnostics)
         local lspconfig = require("lspconfig");
         local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
         -- то, что должно быть в opts
         -- https://neovim.io/doc/user/map.html#%3Amap-arguments
-        local opts = {}
+        local options = {}
 
         -- Функция on_attach используется тут для настройки сочетаний клавиш после того,
         -- как языковой сервер подключается к текущему буферу
         local on_attach = function(_, bufnr)
-            opts.buffer = bufnr
+            options.buffer = bufnr
 
             local builtin = require('telescope.builtin')
 
             -- show defenitions, references
-            opts.desc = "Show LSP references"
-            vim.keymap.set("n", "gr", builtin.lsp_references, opts)
+            options.desc = "Show LSP references"
+            vim.keymap.set("n", "gr", builtin.lsp_references, options)
 
             -- go to declaration
-            opts.desc = "Go to declaration"
-            vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+            options.desc = "Go to declaration"
+            vim.keymap.set("n", "gD", vim.lsp.buf.declaration, options)
 
             -- show lsp definitions
-            opts.desc = "Show LSP definitions"
-            vim.keymap.set("n", "gd", builtin.lsp_definitions, opts)
+            options.desc = "Show LSP definitions"
+            vim.keymap.set("n", "gd", builtin.lsp_definitions, options)
 
             -- show lsp implementations
-            opts.desc = "Show LSP implementations"
-            vim.keymap.set("n", "gi", builtin.lsp_implementations, opts)
+            options.desc = "Show LSP implementations"
+            vim.keymap.set("n", "gi", builtin.lsp_implementations, options)
 
             -- show lsp type definitions
-            opts.desc = "Show LSP type definitions"
-            vim.keymap.set("n", "gt", builtin.lsp_type_definitions, opts)
+            options.desc = "Show LSP type definitions"
+            vim.keymap.set("n", "gt", builtin.lsp_type_definitions, options)
 
             -- smart rename
-            opts.desc = "Smart rename"
-            vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+            options.desc = "Smart rename"
+            vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, options)
 
             -- show buffer diagnostics for file
-            opts.desc = "Show file's diagnostics"
-            vim.keymap.set("n", "<leader>D", builtin.diagnostics, opts)
+            options.desc = "Show file's diagnostics"
+            vim.keymap.set("n", "<leader>D", builtin.diagnostics, options)
 
             -- show buffer diagnostics for line
-            opts.desc = "Show line's diagnostics"
-            vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
+            options.desc = "Show line's diagnostics"
+            vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, options)
 
             -- show documentation for object under cursor
-            opts.desc = "Show documentation for object under cursor"
-            vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+            options.desc = "Show documentation for object under cursor"
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, options)
 
             -- restart lsp
-            opts.desc = "Restart LSP"
-            vim.keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts)
+            options.desc = "Restart LSP"
+            vim.keymap.set("n", "<leader>rs", ":LspRestart<CR>", options)
         end
 
         -- used to enable autocompletion (assign to every lsp server config)
         local capabilities = cmp_nvim_lsp.default_capabilities()
-
-        -- change the Diagnostic symbols in the sign column (gutter)
-        local signs = { Error = " ", Warn  = " ", Hint  = " ", Info  = " ", }
-        for type, icon in pairs(signs) do
-         local hl = "DiagnosticSign" .. type
-         vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-        end
-
+        --
         -- https://github.com/golang/tools/tree/master/gopls
         lspconfig["gopls"].setup({
             capabilities = capabilities,
@@ -116,72 +165,6 @@ return {
             }
         })
 
-        -- https://clangd.llvm.org/installation#compile_commandsjson
-        -- https://clang.llvm.org/docs/ClangCommandLineReference.html
-        lspconfig["clangd"].setup({
-            on_attach = on_attach,
-            cmd = {
-                "clangd",
-                "-log=error",
-                "--background-index",
-                "--limit-results=500",
-                "--completion-style=detailed",
-            }
-        })
-
-        -- https://projects.eclipse.org/projects/eclipse.jdt.ls
-        lspconfig["jdtls"].setup({
-            on_attach = on_attach,
-        })
-
-        -- https://github.com/rust-lang/rust-analyzer
-        -- lspconfig["rust_analyzer"].setup({
-        --     on_attach = on_attach,
-        --     settings = {
-        --         ['rust-analyzer'] = {
-        --             diagnostics = {
-        --                 enable = true,
-        --             }
-        --         }
-        --     }
-        -- })
-        lspconfig["rust_analyzer"].setup{
-            settings = {
-                ["rust-analyzer"] = {
-                    imports = {
-                        granularity = {
-                            group = "module",
-                        },
-                        prefix = "self",
-                    },
-                    cargo = {
-                        allFeatures = true,
-                        loadOutDirsFromCheck = true,
-                        runBuildScripts = true,
-                    },
-                    checkOnSave = {
-                        enable = true,
-                        command = "clippy",
-                        extraArgs = { "--no-deps" },
-                    },
-                    diagnostics = {
-                        enable = true,
-                        experimental = {
-                            enable = true,
-                        },
-                    },
-                    procMacro = {
-                        enable = true,
-                        ignored = {
-                            ["async-trait"] = { "async_trait" },
-                            ["napi-derive"] = { "napi" },
-                            ["async-recursion"] = { "async_recursion" },
-                        },
-                    },
-                },
-            },
-        }
-
         lspconfig["lua_ls"].setup({
             capabilities = capabilities,
             on_attach = on_attach,
@@ -195,6 +178,7 @@ return {
                         -- заставляем lsp увидеть файлы времени исполнения
                         library = {
                             [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+                            [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
                             [vim.fn.stdpath("config") .. "/lua"] = true,
                         }
                     }
@@ -202,17 +186,17 @@ return {
             }
         })
 
-        local htmlCapabilities = vim.lsp.protocol.make_client_capabilities()
-        htmlCapabilities.textDocument.completion.completionItem.snippetSupport = true
-
-        lspconfig["html"].setup({
-            capabilities = htmlCapabilities,
+        -- https://clangd.llvm.org/installation#compile_commandsjson
+        -- https://clang.llvm.org/docs/ClangCommandLineReference.html
+        lspconfig["clangd"].setup({
             on_attach = on_attach,
-        })
-
-        lspconfig["sqls"].setup({
-            capabilities = capabilities,
-            on_attach = on_attach,
+            cmd = {
+                "clangd",
+                "-log=error",
+                "--background-index",
+                "--limit-results=500",
+                "--completion-style=detailed",
+            }
         })
     end
 }
