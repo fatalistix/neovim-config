4903a0fe7800d6b3ff5f5018574953811f7e2e99
fatalistix <slavabal11@outlook.com>
2024-06-13 18:27:15 +0700

    removed commented code

--- lua/fatalistix/plugins/nvim-cmp.lua
+++ lua/fatalistix/plugins/nvim-cmp.lua
@@ -19,7 +19,6 @@ return {
         local defaults = require("cmp.config.default")()
         local lspkind = require("lspkind")
         local luasnip = require("luasnip")
-        -- require("luasnip.loaders.from_vscode").lazy_load()
 
         return {
             completion = {
@@ -71,7 +70,6 @@ return {
                 { name = "luasnip" },
                 { name = "path" },
                 { name = "buffer" },
-                { name = "crates" },
                 { name = "codeium" },
             }),
             formatting = {
@@ -79,8 +77,7 @@ return {
                     maxwidth = 50,
                     ellipsis_char = "...",
                 })
-            }
-            ,
+            },
             experimental = {
                 ghost_text = {
                     hl_group = "CmpGhostText"
@@ -94,50 +91,6 @@ return {
             }
         }
     end,
-    -- config = function()
-    --     local cmp = require("cmp")
-    --     local luasnip = require("luasnip")
-    --     local lspkind = require("lspkind")
-    --
-    --     -- loads vscode style snippets from installed plugins (e. g. friendly-snippets)
-    --     require("luasnip.loaders.from_vscode").lazy_load()
-    --
-    --     cmp.setup({
-    --         -- completion = {
-    --         --     completeopt = "menu,menuone,preview,noselect",
-    --         -- },
-    --         snippet = {
-    --             expand = function(args)
-    --                 luasnip.lsp_expand(args.body)
-    --             end,
-    --         },
-    --         mapping = cmp.mapping.preset.insert({
-    --             ["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
-    --             ["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
-    --             ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-    --             ["<C-f>"] = cmp.mapping.scroll_docs(4),
-    --             ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestion
-    --             ["<C-e>"] = cmp.mapping.abort(), -- close completion window
-    --             ["<CR>"] = cmp.mapping.confirm(),
-    --         }),
-    --
-    --         -- sources for autocompletion
-    --         sources = cmp.config.sources({
-    --             { name = "nvim_lsp" },
-    --             { name = "luasnip" }, -- snippets
-    --             { name = "buffer" }, -- text within current buffer
-    --             { name = "path" }, -- file system paths
-    --         }),
-    --
-    --         formatting = {
-    --             format = lspkind.cmp_format({
-    --                 maxwidth = 50,
-    --                 ellipsis_char = "...",
-    --             }),
-    --         },
-    --     })
-    -- end
-
     -- @param opts cmp.ConfigSchema
     config = function(_, opts)
         for _, source in ipairs(opts.sources) do
