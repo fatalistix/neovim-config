25af4ed91c33c9e0b808ee95e2c60c8e4197b42e
fatalistix <slavabal11@outlook.com>
2024-06-13 18:08:55 +0700

    removed unused keys section

--- lua/fatalistix/plugins/luasnip.lua
+++ lua/fatalistix/plugins/luasnip.lua
@@ -9,17 +9,6 @@ return {
         history = true,
         updateevents = "TextChanged,TextChangedI",
     },
-    keys = {
-        {
-            "<tab>",
-            function()
-                return require("luasnip").jumpable(1) and "<Plug>luasnip-jump-next" or "<tab>"
-            end,
-            expr = true, silent = true, mode = "i",
-        },
-        { "<tab>", function() require("luasnip").jump(1) end, mode = "s" },
-        { "<s-tab>", function() require("luasnip").jump(-1) end, mode = { "i", "s" } },
-    },
     config = function()
         require("luasnip").setup()
         require("luasnip.loaders.from_vscode").lazy_load()
