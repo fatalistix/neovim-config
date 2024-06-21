0fd3f78db41212dc7e8be840893767a73895125e
fatalistix <slavabal11@outlook.com>
2024-06-21 17:25:36 +0700

    added jdtls plugin (not configured)

---
+++ lua/fatalistix/plugins/lang/jdtls.lua
@@ -0,0 +1,16 @@
+-- Плагин для lsp для java
+return {
+    'mfussenegger/nvim-jdtls',
+    config = function()
+        local config = {
+            cmd = { '/usr/bin/jdtls' },
+            root_dir = vim.fs.dirname(
+                vim.fs.find(
+                    {'gradlew', '.git', 'mvnw'},
+                    { upward = true }
+                )[1]
+            ),
+        }
+        require('jdtls').start_or_attach(config)
+    end
+}
