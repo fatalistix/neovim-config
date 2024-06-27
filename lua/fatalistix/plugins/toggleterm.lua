75e9fbbe18d11499ccb00659ed7a9be3aefb2cfa
fatalistix <slavabal11@outlook.com>
2024-06-28 00:44:57 +0700

    added mappings for fast terminal toggle

--- lua/fatalistix/plugins/toggleterm.lua
+++ lua/fatalistix/plugins/toggleterm.lua
@@ -1,5 +1,14 @@
 return {
     'akinsho/toggleterm.nvim',
     version = '*',
-    config = true,
+    opts = {
+
+    },
+    config = function(_, opts)
+        require('toggleterm').setup(opts)
+
+        vim.keymap.set('n', '<leader>tf', ":ToggleTerm direction=float<cr>", { noremap = true, desc = 'Float terminal' })
+        vim.keymap.set('n', '<leader>th', ":ToggleTerm direction=horizontal size=20<cr>", { noremap = true, desc = 'Horizontal terminal' })
+        vim.keymap.set('n', '<leader>tv', ":ToggleTerm direction=vertical size=70<cr>", { noremap = true, desc = 'Vertical terminal' })
+    end,
 }
