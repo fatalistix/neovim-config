2100dffcf789dc4ea2ea008c47de8e716ce3d30a
fatalistix <slavabal11@outlook.com>
2024-06-27 19:17:26 +0700

    extended mappings

--- lua/fatalistix/plugins/undotree.lua
+++ lua/fatalistix/plugins/undotree.lua
@@ -1,8 +1,11 @@
--- дерево отката изменений 
+-- дерево отката изменений
 -- https://github.com/mbbill/undotree
 return {
-    'mbbill/undotree', 
+    'mbbill/undotree',
     config = function()
-        vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle, { desc = "Toggle undotree" })
+        vim.keymap.set('n', '<leader>ut', ":UndotreeToggle<CR>", { desc = "Toggle undotree" })
+        vim.keymap.set('n', '<leader>uo', ":UndotreeShow<CR>", { desc = "Open undotree" })
+        vim.keymap.set('n', '<leader>uc', ":UndotreeHide<CR>", { desc = "Close undotree" })
+        vim.keymap.set('n', '<leader>uf', ":UndotreeFocus<CR>", { desc = "Focus undotree" })
     end
 }
