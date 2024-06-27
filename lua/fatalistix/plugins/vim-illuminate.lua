3c3ff048b40ec6db2b3c3a54a152230c1c6820cd
fatalistix <slavabal11@outlook.com>
2024-06-27 17:05:58 +0700

    changed mappings, decreased delay, removed useless mappings

--- lua/fatalistix/plugins/vim-illuminate.lua
+++ lua/fatalistix/plugins/vim-illuminate.lua
@@ -3,7 +3,7 @@
 return {
     "RRethy/vim-illuminate",
     opts = {
-        delay = 200,
+        delay = 100,
         large_file_cutoff = 2000,
         large_file_overrides = {
             providers = { "lsp" },
@@ -15,22 +15,18 @@ return {
         local function map(key, dir, buffer)
             vim.keymap.set("n", key, function()
                 require("illuminate")["goto_" .. dir .. "_reference"](false)
-            end, { desc = dir:sub(1, 1):upper() .. dir:sub(2) .. " Reference", buffer = buffer })
+            end, { desc = dir:sub(1, 1):upper() .. dir:sub(2) .. " reference", buffer = buffer })
         end
 
-        map("]]", "next")
-        map("[[", "prev")
+        map("]r", "next")
+        map("[r", "prev")
 
         vim.api.nvim_create_autocmd("FileType", {
             callback = function()
                 local buffer = vim.api.nvim_get_current_buf()
-                map("]]", "next", buffer)
-                map("[[", "next", buffer)
+                map("]r", "next", buffer)
+                map("[r", "prev", buffer)
             end,
         })
     end,
-    keys = {
-        { "]]", desc = "Next Reference" },
-        { "[[", desc = "Prev Reference" },
-    }
 }
