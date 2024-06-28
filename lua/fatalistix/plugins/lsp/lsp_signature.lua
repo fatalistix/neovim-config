7e31c79521c773884449f0b0cdca1a373842efd7
fatalistix <slavabal11@outlook.com>
2024-06-28 22:20:10 +0700

    removed useless mapping, fixed mapping conflict

--- lua/fatalistix/plugins/lsp/lsp_signature.lua
+++ lua/fatalistix/plugins/lsp/lsp_signature.lua
@@ -11,12 +11,8 @@ return {
         local lsp_signature = require('lsp_signature')
         lsp_signature.setup(opts)
 
-        vim.keymap.set({ 'n' }, '<C-k>', function()
+        vim.keymap.set({ 'n' }, '<Leader>k', function()
             require('lsp_signature').toggle_float_win()
         end, { silent = true, noremap = true, desc = 'toggle signature' })
-
-        vim.keymap.set({ 'n' }, '<Leader>k', function()
-            vim.lsp.buf.signature_help()
-        end, { silent = true, noremap = true, desc = 'show signature' })
     end
 }
