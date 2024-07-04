f5e0e0068ff17180473b3798e618f535e9ea54a1
fatalistix <slavabal11@outlook.com>
2024-07-04 21:02:34 +0700

    removed commented lines

--- lua/fatalistix/keymaps.lua
+++ lua/fatalistix/keymaps.lua
@@ -1,9 +1,3 @@
--- Resize window using <ctrl> + arrow keys
--- vim.keymap.set("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
--- vim.keymap.set("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
--- vim.keymap.set("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
--- vim.keymap.set("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
-
 -- Move Lines
 -- vim.keymap.set("n", "<C-J>", "<cmd>m .+1<cr>==", { desc = "Move down" })
 -- vim.keymap.set("n", "<C-K>", "<cmd>m .-2<cr>==", { desc = "Move up" })
