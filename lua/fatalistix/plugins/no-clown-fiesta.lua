882eb0b675bcebd363b841e57714129ee774994b
fatalistix <slavabal11@outlook.com>
2024-07-04 23:45:11 +0700

    added custom vim-illuminate support

--- lua/fatalistix/plugins/no-clown-fiesta.lua
+++ lua/fatalistix/plugins/no-clown-fiesta.lua
@@ -2,9 +2,27 @@
 -- https://github.com/aktersnurra/no-clown-fiest.nvim
 return {
     'aktersnurra/no-clown-fiesta.nvim',
+    dependencies = {
+        "RRethy/vim-illuminate",
+    },
     priority = 1000,
     config = function()
         require("no-clown-fiesta").setup()
         vim.cmd.colorscheme("no-clown-fiesta")
+
+        local palette = require("no-clown-fiesta.palette")
+
+        local highlights = {
+            IlluminatedWord =      { bg = palette.accent, },
+            IlluminatedCurWord =   { bg = palette.accent, },
+            IlluminatedWordText =  { bg = palette.accent, },
+            IlluminatedWordRead =  { bg = palette.accent, },
+            IlluminatedWordWrite = { bg = palette.accent, },
+        }
+
+        for group, value in pairs(highlights) do
+            vim.api.nvim_set_hl(0, group, value)
+        end
+
     end
 }
