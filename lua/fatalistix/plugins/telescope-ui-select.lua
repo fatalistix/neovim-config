9a5941438d75b63ae9ab61f4c193d7bef7306597
fatalistix <slavabal11@outlook.com>
2024-07-07 00:29:18 +0700

    added telescope backend for vim.select (disabled)

---
+++ lua/fatalistix/plugins/telescope-ui-select.lua
@@ -0,0 +1,7 @@
+-- vim.select provider in telescope style
+-- https://github.com/nvim-telescope/telescope-ui-select.nvim
+return {
+    'nvim-telescope/telescope-ui-select.nvim',
+    enabled = false,
+    opts = {},
+}
