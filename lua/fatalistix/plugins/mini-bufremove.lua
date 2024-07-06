d5eb25ef605ad932a7cbd55e2abd218b3550e130
fatalistix <slavabal11@outlook.com>
2024-07-06 18:50:26 +0700

    added related mappings for closing buffers

--- lua/fatalistix/plugins/mini-bufremove.lua
+++ lua/fatalistix/plugins/mini-bufremove.lua
@@ -2,4 +2,16 @@
 return {
     "echasnovski/mini.bufremove",
     version = '*',
+    config = function(_, opts)
+        local bufremove = require("mini.bufremove")
+        bufremove.setup(opts)
+
+        vim.keymap.set('n', '<leader>bd', function(bufnr)
+            bufremove.delete(bufnr, false)
+        end, { desc = "Close buffer" })
+
+        vim.keymap.set('n', '<leader>bc', function(bufnr)
+            bufremove.delete(bufnr, false)
+        end, { desc = "Close buffer" })
+    end,
 }
