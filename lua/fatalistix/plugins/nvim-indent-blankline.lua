261c1a7faa200cf8ed6c7528b3ab112723d90e46
fatalistix <slavabal11@outlook.com>
2024-07-07 19:17:14 +0700

    added englisn desc and link to github

--- lua/fatalistix/plugins/nvim-indent-blankline.lua
+++ lua/fatalistix/plugins/nvim-indent-blankline.lua
@@ -1,4 +1,6 @@
 -- цветные и не очень отступы как в ide (области видимости и всякое подобное)
+-- colored indentes like in IDEs
+-- https://github.com/lukas-reineke/indent-blankline.nvim
 return {
     'lukas-reineke/indent-blankline.nvim',
     event = { 'BufReadPost', 'BufWritePost', 'BufNewFile' },
