1b673c684b6c5bb971ea1fa3640a2d2f6d1d10c7
fatalistix <slavabal11@outlook.com>
2024-07-07 19:23:26 +0700

    added nvim-treesitter dependencies

--- lua/fatalistix/plugins/nvim-indent-blankline.lua
+++ lua/fatalistix/plugins/nvim-indent-blankline.lua
@@ -5,5 +5,8 @@ return {
     'lukas-reineke/indent-blankline.nvim',
     event = { 'BufReadPost', 'BufWritePost', 'BufNewFile' },
     main = 'ibl',
+    dependencies = {
+        'nvim-treesitter/nvim-treesitter',
+    },
     opts = {},
 }
