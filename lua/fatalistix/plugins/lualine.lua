388eb7ea09819d2599fce83eccfd2ab8faa93668
fatalistix <slavabal11@outlook.com>
2024-07-07 23:18:15 +0700

    changed separators

--- lua/fatalistix/plugins/lualine.lua
+++ lua/fatalistix/plugins/lualine.lua
@@ -9,8 +9,10 @@ return {
     opts = {
         options = {
             icons_enabled = true,
-            component_separators = { left = '', right = '' },
-            section_separators = { left = '', right = '' },
+            -- component_separators = { left = '', right = '' },
+            component_separators = { left = '', right = '' },
+            -- section_separators = { left = '', right = '' },
+            section_separators = { left = '', right = '' },
             disabled_filetypes = {
                 statusline = {},
                 winbar = {},
@@ -25,7 +27,7 @@ return {
             },
         },
         sections = {
-            lualine_a = { 'mode' },
+            lualine_a = { { 'mode', separator = { left = '' }, right_padding = 2 } },
             lualine_b = { 'branch', 'diff', 'diagnostics' },
             lualine_c = { 'filename' },
             lualine_x = { 'encoding', 'fileformat', 'filetype' },
