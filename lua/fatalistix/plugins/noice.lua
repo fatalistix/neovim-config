865f52860e4831f9f544bccff590cfbe59135a32
fatalistix <slavabal11@outlook.com>
2024-07-08 13:09:27 +0700

    added experimental mappings

--- lua/fatalistix/plugins/noice.lua
+++ lua/fatalistix/plugins/noice.lua
@@ -25,4 +25,50 @@ return {
             lsp_doc_border = false,
         },
     },
+    keys = {
+        {
+            '<leader>snl',
+            function()
+                require('noice').cmd('last')
+            end,
+            desc = 'Noice last message',
+        },
+        {
+            '<leader>snh',
+            function()
+                require('noice').cmd('history')
+            end,
+            desc = 'Noice history',
+        },
+        {
+            '<leader>sna',
+            function()
+                require('noice').cmd('all')
+            end,
+            desc = 'Noice all',
+        },
+        {
+            '<leader>snd',
+            function()
+                require('noice').cmd('dismiss')
+            end,
+            desc = 'Dismiss All',
+        },
+        {
+            '<leader>snt',
+            function()
+                require('noice').cmd('pick')
+            end,
+            desc = 'Noice Picker (Telescope/FzfLua)',
+        },
+    },
+    config = function(_, opts)
+        -- HACK: noice shows messages from before it was enabled,
+        -- but this is not ideal when Lazy is installing plugins,
+        -- so clear the messages in this case.
+        if vim.o.filetype == 'lazy' then
+            vim.cmd([[messages clear]])
+        end
+        require('noice').setup(opts)
+    end,
 }
