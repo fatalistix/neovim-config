1953e4fcfc3938caee07da81fd87526d5bfa1a32
fatalistix <slavabal11@outlook.com>
2024-07-11 00:43:34 +0700

    reorganized code, added mapping, extended options

--- lua/fatalistix/plugins/nvim-tree.lua
+++ lua/fatalistix/plugins/nvim-tree.lua
@@ -2,20 +2,34 @@
 -- https://github.com/nvim-tree/nvim-tree.lua
 return {
     'nvim-tree/nvim-tree.lua',
-    dependencies = 'nvim-tree/nvim-web-devicons',
-    config = function()
-        local function on_attach(bufnr)
+    dependencies = {
+        'nvim-tree/nvim-web-devicons',
+    },
+    opts = function()
+        local on_attach = function(bufnr)
             local api = require('nvim-tree.api')
 
             local function opts(desc)
                 return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
             end
 
+            -- default mappings
             api.config.mappings.default_on_attach(bufnr)
         end
 
-        require('nvim-tree').setup({
+        return {
             on_attach = on_attach,
-        })
+            actions = {
+                open_file = {
+                    resize_window = false, -- don't resize window when opening file
+                },
+            },
+        }
+    end,
+    config = function(_, opts)
+        require('nvim-tree').setup(opts)
+
+        -- keys section doesn't work for this plugin
+        vim.keymap.set('n', '<leader>e', '<Cmd>NvimTreeToggle<CR>', { desc = 'Open nvim-tree' })
     end,
 }
