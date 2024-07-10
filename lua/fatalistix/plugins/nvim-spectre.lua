52cfe0d81f4fc7b0fb389eea66ab755c0f6995e6
fatalistix <slavabal11@outlook.com>
2024-07-10 23:57:45 +0700

    extended mappings

--- lua/fatalistix/plugins/nvim-spectre.lua
+++ lua/fatalistix/plugins/nvim-spectre.lua
@@ -10,11 +10,36 @@ return {
     opts = { open_cmd = 'noswapfile vnew' },
     keys = {
         {
-            '<leader>sr',
+            '<leader>srt',
             function()
-                require('spectre').open()
+                require('spectre').toggle()
             end,
-            desc = 'Replace in files (Sprectre)',
+            desc = 'Toggle search and replace in multiple files',
+            mode = 'n',
+        },
+        {
+            '<leader>srw',
+            function()
+                require('spectre').open_visual({ select_word = true })
+            end,
+            desc = 'Search current word',
+            mode = 'n',
+        },
+        {
+            '<leader>srw',
+            function()
+                require('spectre').open_visual()
+            end,
+            desc = 'Search current word',
+            mode = 'v',
+        },
+        {
+            '<leader>srp',
+            function()
+                require('spectre').open_file_search({ select_word = true })
+            end,
+            desc = 'Search on current file',
+            mode = 'n',
         },
     },
 }
