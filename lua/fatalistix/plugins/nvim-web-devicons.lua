2b7e92567c68ac5f709abeb7785c60130d62b2f2
fatalistix <slavabal11@outlook.com>
2024-07-11 22:17:35 +0700

    moved from config to opts

--- lua/fatalistix/plugins/nvim-web-devicons.lua
+++ lua/fatalistix/plugins/nvim-web-devicons.lua
@@ -2,7 +2,7 @@
 -- https://github.com/kyazdani42/nvim-web-devicons
 return {
     'kyazdani42/nvim-web-devicons',
-    config = function()
-        require('nvim-web-devicons').setup({ default = true })
-    end,
+    opts = {
+        default = true,
+    },
 }
